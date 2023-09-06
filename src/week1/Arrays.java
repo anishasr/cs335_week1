050c57bf6f363e87f098e05990e79a90c90694a1
“anishasr” <srivasta@simmons.edu>
2023-09-06 16:23:53 -0500

    created new classes

---
+++ src/week1/Arrays.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Arrays {
+	
+	public static void main(String args[]) {
+		String[] flowers = {"roses", "sunflowers", "carnations"};
+		
+		// print array length
+		System.out.println(flowers.length);
+		
+		// loop through array
+		for(int i=0;i<flowers.length;i++) {
+			System.out.println(flowers[i]);
+		}
+	}
+
+
+}
